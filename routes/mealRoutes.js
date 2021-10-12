186a1be72dea79d3a82ab66901841bbdee92b1e7
Snehendu Roy <roysnehendupersonal@gmail.com>
2021-10-12 15:45:45 +0530

    Router Session done

--- routes/mealRoutes.js
+++ routes/mealRoutes.js
@@ -4,5 +4,7 @@ const getUser = require("../middleware/getuser");
 
 router.post("/", getUser, mealsCtrl.create);
 router.get("/", mealsCtrl.read);
+router.patch("/:id", mealsCtrl.update);
+router.delete("/:id", mealsCtrl.delete);
 
 module.exports = router;
